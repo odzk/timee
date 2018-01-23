f9e41e33b59339ad4a1d06562f04fc2a0a14ac38
beansbeans <beanssaenz@gmail.com>
2018-01-23 13:34:20 +0800

    teachers dashboard modification

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180118093544) do
+ActiveRecord::Schema.define(version: 20180119033925) do
 
   create_table "adds", force: :cascade do |t|
     t.datetime "created_at", null: false
